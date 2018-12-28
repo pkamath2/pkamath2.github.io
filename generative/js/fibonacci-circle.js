602dbe08bd40c9faaf357e054e5d885b4c884856
pkamath2 <git.pkamath2@gmail.com>
2018-12-28 21:56:35 +0800

    Animation

--- generative/js/fibonacci-circle.js
+++ generative/js/fibonacci-circle.js
@@ -1,69 +1,77 @@
 var fibo;
 var canvas;
+var index = 0;
+var angle = 0;
+var count = 0;
+
+var angleChange = 0;
+var increment = 0;
+
 function setup() {
-    fibo = createSeries(700);
+    fibo = createSeries(600);
     canvas = createCanvas(1000, 900);
     canvas.position(345,55);
-    noLoop();
-    
+    background('black');
+    frameRate(150);
+
+    // Original values from Shiffmann's blog - Did not produce cool results with my code. 
+    // Better looking values below. 
+    // WARNING: Convert to radians.
+    // angleChange = 137.5;
+    // increment = 200;
+
+    // Good ones
+    // angleChange = 0.0174533;/*Interesting. 1 deg*/
+    // increment = 200;
+
+    // angleChange = Math.PI/25;/*Basic. Ok-ish. Use this with createSeries(100)*/
+    // increment = width;
+
+    // angleChange = (Math.PI)+10;/*Ok-ish Awesome. Use this with createSeries(500) or createSeries(150)*/
+    // increment = width/4;
+
+    // angleChange = Math.PI/4;/*Ok-ish Awesome. Use this with createSeries(500)*/
+    // increment = width/4;
+
+    // angleChange = Math.PI/9;/*Cool! Use this with createSeries(500)*/
+    // increment = width/4;
+
+    // angleChange = Math.PI + Math.PI/1000;/*Interesting. 180.018 deg*/
+    // increment = width/4;
+
+    // angleChange = Math.PI + Math.PI/444;/*Interesting. 180.32 deg*/
+    // increment = width/4;
+
+    // angleChange = Math.PI/4;/*Cool. 45 deg*. Try with createSeries(1000)*/
+    // increment = 200;
+
+    // angleChange = 7.67945;/*Interesting. 440 deg*/
+    // increment = 100;
+
+    angleChange = 0.418879;/*AMAZING!!!!!!. 24 deg. Use createSeries(600)*/
+    increment = 100;
   }
   
   function draw() {
-    background('black');
+    
     noFill();
     stroke('white');
     strokeWeight(1.5);
     translate(width/2, height/2);
-    // Original values from Shiffmann's blog - Did not produce cool results with my code. 
-    // Better looking values below. 
-    // WARNING: Convert to radians.
-    // var angleChange = 137.5;
-    // var increment = 200;
+    
+    var r = Math.sqrt(count)
+    //r = count;
+    var x = r * sin(angle);
+    var y = r * cos(angle);
+    
+    angle += angleChange;
+    count += increment;
+    ellipse(x, y, Math.log(fibo[index]));
 
-    // Good ones
-    // var angleChange = 0.0174533;/*Interesting. 1 deg*/
-    // var increment = 200;
-
-    // var angleChange = Math.PI/25;/*Basic. Ok-ish. Use this with createSeries(400)*/
-    // var increment = width;
-
-    // var angleChange = (Math.PI)+10;/*Ok-ish Awesome. Use this with createSeries(500) or createSeries(150)*/
-    // var increment = width/4;
-
-    // var angleChange = Math.PI/4;/*Ok-ish Awesome. Use this with createSeries(500)*/
-    // var increment = width/4;
-
-    // var angleChange = Math.PI/9;/*Cool! Use this with createSeries(500)*/
-    // var increment = width/4;
-
-    // var angleChange = Math.PI + Math.PI/1000;/*Interesting. 180.018 deg*/
-    // var increment = width/4;
-
-    // var angleChange = Math.PI + Math.PI/444;/*Interesting. 180.32 deg*/
-    // var increment = width/4;
-
-    // var angleChange = Math.PI/4;/*Cool. 45 deg*. Try with createSeries(1000)*/
-    // var increment = 200;
-
-    // var angleChange = 7.67945;/*Interesting. 440 deg*/
-    // var increment = 100;
-
-    var angleChange = 0.418879;/*AMAZING!!!!!!. 24 deg*/
-    var increment = 100;
-
-
-    var angle = 0;
-    var count = 0;
-    fibo.forEach((f,ind) => {
-        var r = Math.sqrt(count)
-        //r = count;
-        var x = r * sin(angle);
-        var y = r * cos(angle);
-        
-        angle += angleChange;
-        count += increment;
-        ellipse(x, y, Math.log(f));
-    });
+    index++;
+    if(index == fibo.length){
+        noLoop();
+    }
   }
 
   function createSeries(limit){
