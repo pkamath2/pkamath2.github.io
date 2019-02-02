78eb158aa0009c77a6818860a3257f17f1560df6
pkamath2 <git.pkamath2@gmail.com>
2019-02-02 10:36:22 +0800

    Right click fix.

--- generative/tiling/tiling-wave.js
+++ generative/tiling/tiling-wave.js
@@ -1,20 +1,21 @@
-let height = 1000;
-let width = 700;
+let height = 700;
+let width = 1500;
 let size = 10;
 let colors = ['black']
 let density = 0.02;
 
 function setup(){
     canvas = createCanvas(width,height);
-    canvas.position(475,55);
+    //canvas.position(475,55);
+    canvas.position(0,55);
     noLoop();
     background('gray')
 }
 
 
 function draw(){
-    for(var i=0;i<width/size; i++){
-       for(var j=0;j<height/size; j++){
+    for(var i=0;i<height/size; i++){
+       for(var j=0;j<width/size; j++){
             tile(i*size, j*size, random([0,1,2,3]))
        }
     }
@@ -29,7 +30,7 @@ function tile(x, y, orientation){
     push();
     noStroke();
     fill(random(colors));
-    translate(x, y);
+    translate(y,x);
     translate(size/2, size/2);
     rotate(angle);
     translate(-size/2, -size/2);
@@ -41,7 +42,7 @@ function tile(x, y, orientation){
         push()
         noStroke();
         fill(random(colors));
-        translate(x, y);
+        translate(y,x);
         translate(size/2, size/2);
         rotate(s_angle);
         translate(-size/2, -size/2);
