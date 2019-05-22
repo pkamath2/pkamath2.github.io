cf619116a354c7e7abf74e52b8ad24e13cd13431
pkamath2 <git.pkamath2@gmail.com>
2019-05-22 15:24:08 +0800

    More fibo with D3

---
+++ generative/js/fibonacci-circle-svg.js
@@ -0,0 +1,51 @@
+var fibo;
+var canvas;
+var index = 0;
+var angle = 0;
+var count = 0;
+
+var angleChange =  Math.PI + Math.PI/1000
+var increment = 200;
+
+function draw() {
+  fibo = createSeries(300);
+
+  var svg = d3.select("#fibo");
+  svg.attr("transform", "translate(400, 0)")
+  const width = +svg.attr('width');
+  const height = +svg.attr('height');
+
+  var g = svg.append("g");
+  g.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+  g.selectAll("circle").data(fibo).enter().append("circle")
+    .attr("cx", (d, i) => {
+      return Math.sqrt(increment * i) * Math.sin(angleChange * i)
+    })
+    .attr("cy", (d, i) => {
+      return Math.sqrt(increment * i) * Math.cos(angleChange * i)
+    })
+    .attr("r", (d, i) => { if (d == 0) return 0; else return Math.log(d) })
+    .attr("stroke-width", "2")
+    .attr("fill", "None")
+    .attr("stroke", "pink");
+}
+
+draw();
+
+function createSeries(limit) {
+  var series = [];
+  var first = 0;
+  var middle = first + 1;
+  series.push(first);
+  series.push(middle);
+
+  var end;
+  for (i = 0; i < limit; i++) {
+    end = middle + first;
+    series.push(end);
+    first = middle;
+    middle = end;
+  }
+  return series;
+}
